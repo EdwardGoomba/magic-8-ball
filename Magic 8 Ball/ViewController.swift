c9ba7d918b4f42cedfc443b2c514afa2b1f5ecb9
EdwardGoomba <ask@edwardvdesign.com>
2017-10-29 21:32:16 -0400

    Linked image and button to code

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -15,11 +15,15 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBOutlet var ImageView: UIView!
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func askButtonPressed(_ sender: Any) {
+    }
+    
 }
 
